3b9b9c83a324438e8115a3ad8ff5a285301da8c9
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-06-26 14:52:47 +1000

    Minimal test case

--- napari_tifffile/_tests/test_tifffile.py
+++ napari_tifffile/_tests/test_tifffile.py
@@ -1,15 +1,14 @@
 import numpy as np
 from napari_tifffile import napari_get_reader
+import tifffile
 
 
-# tmp_path is a pytest fixture
 def test_reader(tmp_path):
     """An example of how you might test your plugin."""
 
-    # write some fake data using your supported file format
-    my_test_file = str(tmp_path / "myfile.npy")
+    my_test_file = str(tmp_path / "myfile.tif")
     original_data = np.random.rand(20, 20)
-    np.save(my_test_file, original_data)
+    tifffile.imwrite(my_test_file, original_data)
 
     # try to read it back in
     reader = napari_get_reader(my_test_file)

79ca5618bf4d98f5423ca7ff6df52ed6cad317de
Joost de Folter <folterj@gmail.com>
2024-03-16 00:31:25 +0100

    Expanded ome-tiff testing

--- napari_tiff/_tests/test_tiff_reader_metadata.py
+++ napari_tiff/_tests/test_tiff_reader_metadata.py
@@ -1,10 +1,13 @@
-from napari import Viewer
+from glob import glob
 from napari.layers import Layer, Image
 import numpy as np
 import pytest
 import tifffile
 
 from napari_tiff import napari_get_reader
+
+
+viewer = None
 
 
 def generate_ometiff_file(tmp_path, filename, data):
@@ -14,54 +17,60 @@ def generate_ometiff_file(tmp_path, filename, data):
 
 
 @pytest.mark.parametrize("data_fixture, original_filename, original_data", [
-    (generate_ometiff_file, "myfile.ome.tif", np.random.randint(0, 255, size=(20, 20, 3)).astype(np.uint8)),
-    (None, "D:/slides/EM04573_01small.ome.tif", None),
+    (generate_ometiff_file, "myfile.ome.tif", np.random.randint(0, 255, size=(16, 16)).astype(np.uint8)),
+    (generate_ometiff_file, "myfile.ome.tif", np.random.randint(0, 255, size=(16, 16, 3)).astype(np.uint8)),
+    (None, "D:/slides/test/*", None),
     ])
 def test_reader(tmp_path, data_fixture, original_filename, original_data):
+    global viewer
 
     if data_fixture is not None:
-        test_file = data_fixture(tmp_path, original_filename, original_data)
+        test_files = [data_fixture(tmp_path, original_filename, original_data)]
     else:
-        test_file = original_filename
+        test_files = glob(original_filename)
 
-    # try to read it back in
-    reader = napari_get_reader(test_file)
-    assert callable(reader)
+    for test_file in test_files:
+        # try to read it back in
+        reader = napari_get_reader(test_file)
+        assert callable(reader)
 
-    # make sure we're delivering the right format
-    layer_datas = reader(test_file)
-    assert isinstance(layer_datas, list) and len(layer_datas) > 0
+        # make sure we're delivering the right format
+        layer_datas = reader(test_file)
+        assert isinstance(layer_datas, list) and len(layer_datas) > 0
 
-    for layer_data in layer_datas:
-        assert isinstance(layer_data, tuple) and len(layer_data) > 0
+        for layer_data in layer_datas:
+            assert isinstance(layer_data, tuple) and len(layer_data) > 0
 
-        data = layer_data[0]
-        metadata = layer_data[1]
+            data = layer_data[0]
+            metadata = layer_data[1]
 
-        if original_data is not None:
-            # make sure the data is the same as it started
-            np.testing.assert_allclose(original_data, data)
-        else:
-            # test pixel data
-            if isinstance(data, list):
-                data0 = data[0]
+            if original_data is not None:
+                # make sure the data is the same as it started
+                np.testing.assert_allclose(original_data, data)
             else:
-                data0 = data
-            assert data0.size > 0
-            slicing = tuple([0] * data0.ndim)
-            value = np.array(data0[slicing])
-            assert value is not None and value.size > 0
+                # test pixel data
+                if isinstance(data, list):
+                    data0 = data[0]
+                else:
+                    data0 = data
+                assert data0.size > 0
+                slicing = tuple([0] * data0.ndim)
+                value = np.array(data0[slicing])
+                assert value is not None and value.size > 0
 
-        # test layer metadata
-        if 'channel_axis' in metadata:
-            layers = Viewer().add_image(data, **metadata)
-            if not isinstance(layers, list):
-                layers = [layers]
-            for layer in layers:
+            # test layer metadata
+            if 'channel_axis' in metadata:
+                if viewer is None:
+                    from napari import Viewer
+                    viewer = Viewer()
+                layers = viewer.add_image(data, **metadata)
+                if not isinstance(layers, list):
+                    layers = [layers]
+                for layer in layers:
+                    assert isinstance(layer, Image)
+            else:
+                layer = Layer.create(*layer_data)  # incompatible with channel_axis
                 assert isinstance(layer, Image)
-        else:
-            layer = Layer.create(*layer_data)  # incompatible with channel_axis
-            assert isinstance(layer, Image)
 
-            layer = Image(data, **metadata)    # incompatible with channel_axis
-            assert isinstance(layer, Image)
+                layer = Image(data, **metadata)    # incompatible with channel_axis
+                assert isinstance(layer, Image)
